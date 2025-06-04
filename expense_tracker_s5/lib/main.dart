d29f9d93174f18dd05346f13a8942d78ad2385d4
Nickobellic <avrahulkanna17@gmail.com>
2025-06-04 15:31:00 +0530

    Update app bar theming along with color scheme initialization.

--- expense_tracker_s5/lib/main.dart
+++ expense_tracker_s5/lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_s5/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 124, 27, 235),
+);
+
 void main() {
-  runApp(MaterialApp(home: const Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      home: const Expenses(),
+    ),
+  );
 }
